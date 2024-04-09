091d24180a3dd5cc3401b93a5e7a95da6c67095b
Masterwhiece <therealmaria@naver.com>
2024-04-09 21:29:05 +0900

    update

---
+++ Stages/4-1dim_array/BJ10811_ReversingBasket.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BJ10811_ReversingBasket {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] intArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            intArr[i] = i + 1;
+        }
+
+        int start, end;
+        for (int i = 0; i < m; i++) {
+            start = sc.nextInt()-1;
+            end = sc.nextInt()-1;
+            int len = end - start + 1;
+
+            int[] newArr = new int[len];
+            for (int j = 0; j < len; j++) {
+                newArr[j] = intArr[end-j];
+            }
+            for (int j = 0; j < newArr.length; j++) {
+                System.out.print(newArr[j] + " ");
+            }
+            System.out.println();
+
+
+
+            for (int j = 0; j < len; j++) {
+                intArr[start+j] = newArr[j];
+            }
+            for (int j = 0; j < n; j++) {
+                System.out.print(intArr[j] + " ");
+            }
+            System.out.println();
+
+
+
+
+
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(intArr[i] + " ");
+        }
+        sc.close();
+    }
+}
--- Stages/4-1dim_array/BJ3052_Remainder.java
+++ Stages/4-1dim_array/BJ3052_Remainder.java
@@ -1,13 +1,15 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 // 3052 나머지
 public class BJ3052_Remainder {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-
-
+        HashSet<Integer> mySet = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            mySet.add(sc.nextInt() % 42);
+        }
+        System.out.println(mySet.size());
         sc.close();
     }
 }
