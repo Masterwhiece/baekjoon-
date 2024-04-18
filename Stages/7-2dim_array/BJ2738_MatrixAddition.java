028abfe0f359907015ab461d7ca9fc66f669ebc2
Masterwhiece <therealmaria@naver.com>
2024-04-18 14:28:15 +0900

    update

---
+++ Stages/7-2dim_array/BJ2738_MatrixAddition.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class BJ2738_MatrixAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] mat1 = new int[n][m];
+        int[][] mat2 = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat1[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat2[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(mat1[i][j] + mat2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
